abc0650afc6f8510dceab30ed8d4ffde5936848a
larissa dias <laladias2211@gmail.com>
2022-11-30 19:27:11 -0300

    formulario

---
+++ app.js
@@ -0,0 +1,5 @@
+function Salvar(){
+    let nome = document.getElementById("nome").value
+    let sobrenome = document.getElementById("sobrenome").value
+    let linha = "<tr><td>" + nome + "</td><td>" + sobrenome + "</td><tr>"
+}    
